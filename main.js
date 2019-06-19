465f3963aabce11715238ddaa3cf419615136957
Mateusz Hadrian <tenhadrian@gmail.com>
2019-06-19 20:06:52 +0200

    feat: added some JS

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const myName = 'Mateusz';
 const mySurname = 'Hadrian';
 
 console.log(`Dobry wieczór, tutaj ${myName} ${mySurname}. Co tam słychać?`)
+
+const AboutParagraph3 = document.querySelector('.about__paragraph--js');
+
+console.log(AboutParagraph3.innerHTML);
