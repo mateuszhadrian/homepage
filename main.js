3f40f38c57b8dc9a78543a6dc8ef0c136b661b3b
Mateusz Hadrian <tenhadrian@gmail.com>
2019-06-19 20:41:21 +0200

    feat: added paragraph from JS

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const mySurname = 'Hadrian';
 
 console.log(`Dobry wieczór, tutaj ${myName} ${mySurname}. Co tam słychać?`)
 
-const AboutParagraph3 = document.querySelector('.about__paragraph--js');
+const ThingsParagraphJS = document.querySelector('.things__paragraph--js');
 
-console.log(AboutParagraph3.innerHTML);
+ThingsParagraphJS.innerHTML = `Tutaj jest paragraf dodany z poziomu JavaScript jako proste zadanko domowe. Ciekawe czy będzie się zachowywał dokładnie tak samo jak wszystkie paragrafy powyżej. Przynajmniej powinny. Żeby napisać jeszcze trochę tekstu to wrzucę sobie tutaj zmienną z moim imieniem. Jednocześnie dzięki temu poćwiczę sobie wrzucania zmiennych do stringów. No więc moje imię to ${myName}. Dziękuję Państwu za uwagę. Pisał ${mySurname}.`;
+
